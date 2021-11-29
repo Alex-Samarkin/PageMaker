c2d9c841c2cc328888240a97952eb75392b77379
Александр Иванович Самаркин <alexsamarkinru@gmail.com>
2021-11-29 18:46:11 +0300

    Добавьте файлы проекта.

---
+++ PageMaker/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// Подключаем библиотеку классов, которая предварительно добавлена в ссылки / references
+using PageMakerLib;
+
+namespace PageMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // output and waiting for Enter key
+            Console.WriteLine(About.Description);
+            Console.ReadLine();
+        }
+    }
+}
